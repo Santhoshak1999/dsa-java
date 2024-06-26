9dc87d2a1409b5cb84ac46b3314e8f33bfcc9101
Santhoshak1999 <sk1416986@gmail.com>
2024-06-26 18:25:15 +0530

    create and store in ArrayList

---
+++ Java/Collections/createArrList.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class createArrList {
+    public static void main(String[] args) {
+            //create array list of string
+    ArrayList<String> colors = new ArrayList<String>();
+    //add colors to array list
+    colors.add("Red");
+    colors.add("Green");
+    colors.add("Orange");
+    colors.add("White");
+    colors.add("Black");
+    System.out.println(colors);
+
+    }
+
+    
+}
