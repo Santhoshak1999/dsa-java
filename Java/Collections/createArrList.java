ad34e6c9115be114bbc1bb9189ea39c55c7c5917
Santhoshak1999 <sk1416986@gmail.com>
2024-06-26 18:48:45 +0530

    Insert element at index

--- Java/Collections/createArrList.java
+++ Java/Collections/createArrList.java
@@ -9,10 +9,15 @@ public class createArrList {
     //add colors to array list
     colors.add("Red");
     colors.add("Green");
-    colors.add("Orange");
+    colors.add("Orange");//here string is an objects
     colors.add("White");
     colors.add("Black");
     System.out.println(colors);
+
+    //Print using for loop
+    for(String color : colors){
+        System.out.println(color);
+    }
 
     }
 
