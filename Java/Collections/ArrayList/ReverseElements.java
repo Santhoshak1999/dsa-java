ca784d97c0a71f9a15fc51fc558491520321c71c
Santhoshak1999 <sk1416986@gmail.com>
2024-06-27 15:22:51 +0530

    Reverse Element

---
+++ Java/Collections/ArrayList/ReverseElements.java
@@ -0,0 +1,22 @@
+package Collections.ArrayList;
+
+import java.util.*;
+
+public class ReverseElements {
+    public static void main(String[] args) {
+        List<String> colors = new ArrayList<String>();
+        Collections.addAll(colors, "Red", "White", "Black", "Green", "Yellow");
+        System.out.println("Before reverse the elements");
+        System.out.println(colors);
+        System.out.println();
+
+        //Reverse the Element
+        Collections.reverse(colors);
+        System.out.println("After Reverse the Elements");
+        System.out.println(colors);
+
+    }
+ 
+
+    
+}
